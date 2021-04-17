23917b85468cd45a5f592547d63f9db1b9bdc1a0
Maxime Beacco <Obscure-Angel@gmx.fr>
2021-04-17 19:35:57 +0200

    MàJ de la commande de ban
    Ajout de la requête d'insertion dans la BDD
    Ajout d'un contrôle si le membre n'a pas un ban déjà actif dans son historique

--- commands/ban.js
+++ commands/ban.js
@@ -6,33 +6,90 @@ module.exports = {
 			return po_message.reply('vous devez mentionner quelqu\'un pour le bannir.');
 		}
 		
-		var lo_taggedUser = po_message.mentions.users.first();
-		var lo_guildMemberManager = po_message.guild.members;
+		var fo_taggedUser = po_message.mentions.users.first();
+		var fo_guildMemberManager = po_message.guild.members;
 
-		// Mention de la persionne à bannin
+		/**
+		 * mbeacco - 17/04/2021
+		 * Je retire la mention du membre à bannir du message.
+		 */
 		pa_parametres.shift();
 
 		// Nombre de jours de suppression des messages
-		li_daysDeletedMessages = pa_parametres.shift();
+		var fi_daysDeletedMessages = pa_parametres.shift();
 
 		// Raison invoquée
-		ls_reason = pa_parametres.join(' ');
+		var fs_reason = pa_parametres.join(' ');
+		if (fs_reason.length > 90) {
+			return po_message.reply(' la raison invoquée ne doit pas dépasser 90 caractères. Longueur actuelle : ' + fs_reason.length);
+		}
 
-		lo_guildMemberManager.fetch(lo_taggedUser.id)
+		fo_guildMemberManager.fetch(fo_taggedUser.id)
 			.then(function (po_guildMember){
-				po_guildMember.ban(li_daysDeletedMessages, ls_reason)
-					.then(function (po_bannedGuildMember) {
-						
-						return po_message.reply('tu as banni ' + lo_taggedUser.username + ' ! Les messages envoyés depuis ' + li_daysDeletedMessages + ' jour(s) ont été supprimés.');
-					})
-					.catch(function (po_error) {
-						console.error(po_error.message);
-						return po_message.reply('une erreur est survenue. ' + lo_taggedUser.username + ' n\'a pas été banni.')
-					});
+				
+				var fs_query = "SELECT ban_memberId FROM kb_ban";
+				fs_query += " WHERE ban_memberId = " + po_guildMember.user.id + " AND ban_unbanDate IS NULL";
+
+				/**
+				 * mbeacco - 17/04/2021
+				 * On vérifie que le membre n'est pas déjà banni
+				 */
+				po_connexionKrokmou.query(fs_query, function (po_erreur, po_ligne) {
+					if (po_erreur == undefined && po_ligne.length == 0) {
+
+						/**
+						 * mbeacco - 17/04/2021
+						 * Si le membre n'est pas déjà banni, on insère une ligne
+						 */
+						var fd_dateEnregistrement = new Date();
+					
+						fs_query = "INSERT INTO kb_ban (ban_memberId, ban_memberUsername, ban_reason, ban_daysDeletingMsg, ban_banDate, ban_banModId) VALUES ("; 
+						fs_query += po_guildMember.user.id + ", ";
+						fs_query += "'" + po_guildMember.user.tag + "', ";
+						fs_query += "'" + fs_reason + "', ";
+						fs_query += fi_daysDeletedMessages + ", ";
+						fs_query += "'" + fd_dateEnregistrement.toISOString().slice(0, 19).replace('T', ' ') + "', ";
+						fs_query += po_message.author.id;
+						fs_query += ")";
+
+						po_connexionKrokmou.query(fs_query, function (po_erreur, po_ligne) {
+							if (po_erreur == undefined) {
+
+								/**
+								 * mbeacco - 17/04/2021
+								 * Si l'insertion s'est bien passé, on déclanche le bannissement du membre
+								 */
+								po_guildMember.ban(fi_daysDeletedMessages, fs_reason)
+								.then(function (po_bannedGuildMember) {
+									return po_message.reply('tu as banni ' + po_guildMember.user.username + ' ! Les messages envoyés depuis ' + fi_daysDeletedMessages + ' jour(s) ont été supprimés.');
+								})
+								.catch(function (po_error) {
+									console.error(po_error.message);
+									return po_message.reply('une erreur est survenue. ' + fo_taggedUser.username + ' n\'a pas été banni.')
+								});
+							}
+							else {
+								console.error(po_erreur);
+								return po_message.reply("le ban de " + po_guildMember.user.username + " n'a pas pu être ajouté à son historique. Il ne sera donc pas banni.");
+							}
+						});
+					}
+					else if (po_ligne.length != 0) {
+						/**
+						 * mbeacco - 17/04/2021
+						 * Si il est déjà banni on informe le modérateur
+						 */
+						return po_message.reply('impossible de bannir ' + po_guildMember.user.username + ', il est déjà banni du serveur.');
+					}
+					else {
+						console.error(po_erreur);
+						return po_message.reply("une erreur est survenue lors de la vérification de l'historique de ban. " + po_guildMember.user.username + " n'a pas été banni.");
+					}
+				});
 			})
 			.catch(function (po_error) {
 				console.error(po_error.message);
-				return po_message.reply('une erreur est survenue. ' + lo_taggedUser.username + ' n\'a pas été banni.')
+				return po_message.reply('le membre ' + fo_taggedUser.username + ' n\'a pas été trouvé sur le serveur.')
 			});
 	},
 };
