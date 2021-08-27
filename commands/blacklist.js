7cf08f217802d9df71ee05462d9c3759354762c2
ObscureAngel <Obscure-Angel@gmx.fr>
2021-08-28 00:07:22 +0200

    Create blacklist.js
    Ajout de la commande pour la blacklist

---
+++ commands/blacklist.js
@@ -0,0 +1,8 @@
+module.exports = {
+	fs_nomCommande: 'blacklist',
+	fs_descriptionCommande: 'Blacklist someone',
+	fs_usageCommande: 'blacklist,bl',
+	execute(po_poolConnexionKrokmou, po_message, pa_parametres) {
+		fs_idDeveloppeur = "341645728260685824";
+	}
+}
