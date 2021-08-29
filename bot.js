4e6988640cc07b0bbbb1ccfd09a490e165148737
ObscureAngel <Obscure-Angel@gmx.fr>
2021-08-29 23:49:35 +0200

    Update bot.js
    Update discord.js 13.1.0, node 16.8

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const lo_discord = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
 const lo_fs = require('fs');
 const lo_path = require('path');
 const lo_mysql = require('mysql');
@@ -11,8 +11,28 @@ function fct_getToken(ps_relPath) {
 }
 
 // Création d'une instance d'un client Discord
-var lo_bot = new lo_discord.Client({autoReconnect: true});
-lo_bot.la_commands = new lo_discord.Collection();
+var lo_bot = new Client({
+	autoReconnect: true,
+    allowedMentions: { parse: ['users', 'roles'], repliedUser: true },
+    intents: [
+    Intents.FLAGS.GUILDS, 
+    Intents.FLAGS.GUILD_MEMBERS, 
+    Intents.FLAGS.GUILD_BANS, 
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, 
+    Intents.FLAGS.GUILD_INTEGRATIONS, 
+    Intents.FLAGS.GUILD_WEBHOOKS, 
+    Intents.FLAGS.GUILD_INVITES, 
+    Intents.FLAGS.GUILD_VOICE_STATES, 
+    Intents.FLAGS.GUILD_PRESENCES, 
+    Intents.FLAGS.GUILD_MESSAGES, 
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS, 
+    Intents.FLAGS.GUILD_MESSAGE_TYPING, 
+    Intents.FLAGS.DIRECT_MESSAGES, 
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGE_TYPING
+    ]
+});
+lo_bot.la_commands = new Collection();
 
 // #region Initialisation des variables
 // Préfixe pour les commandes
