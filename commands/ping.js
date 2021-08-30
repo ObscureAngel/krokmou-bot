08d337e9f9da1ee230c80f245eafcb91184eec1e
ObscureAngel <Obscure-Angel@gmx.fr>
2021-08-30 12:22:48 +0200

    Update ping.js
    Passage du ping en secondes

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,6 @@ module.exports = {
 	fs_descriptionCommande: 'Affiche le temps de réponse du bot.',
     fs_usageCommande: 'k!ping',
 	execute(po_connexionKrokmou, po_message, pa_parametres) {
-		po_message.reply('Ping : ' + (po_message.createdAt - Date.now()) + ' millisecondes');
+		po_message.reply('Ping : ' + ((Date.now() - Date.parse(po_message.createdAt))/1000) + ' secondes');
 	},
 };
